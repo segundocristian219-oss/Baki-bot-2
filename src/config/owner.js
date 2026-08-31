36cf6a6867f731e45e71b826672bae8ad3c688b5
segundocristian219-oss <segundocristian219@gmail.com>
2026-08-31 01:25:18 -0600

    Actualizar owner.js

--- src/config/owner.js
+++ src/config/owner.js
@@ -3,6 +3,6 @@ import 'dotenv/config';
 
 global.owner = [['50432955554'], ['5216711089134'], ['15614809253']];
 global.dev1 = ['50432955554'];
-global.developer = '𝙳𝚎𝚢𝚕𝚒𝚗 𝙴𝚕𝚒𝚊𝚌';
+global.developer = 'hrz';
 global.key = process.env.API_KEY;
 global.v = JSON.parse(fs.readFileSync('./package.json', 'utf-8')).version;
