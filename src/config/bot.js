1401ee949d6849090a839830a7a2e717c4f24236
segundocristian219-oss <segundocristian219@gmail.com>
2026-08-31 01:24:59 -0600

    Actualizar bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -1,14 +1,14 @@
 /*global.botNames = [
-  '𝐊𝐚𝐭𝐬𝐮𝐤𝐢 𝐌𝐃',
-  '𝐊𝐚𝐭𝐬𝐮𝐤𝐢 𝐁𝐨𝐭',
-  'ᴋᴀᴛsᴜᴋɪ ᴍᴅ',
-  'ᴋᴀᴛsᴜᴋɪ ʙᴏᴛ',
-  '𝑩𝒂𝒌𝒖𝒈𝒐 𝑴𝑫',
-  '𝑩𝒂𝒌𝒖𝒈𝒐 𝑩𝒐𝒕',
-  'ʙᴀᴋᴜɢᴏ ᴍᴅ',
-  'ʙᴀᴋᴜɢᴏ ʙᴏᴛ',
-  '𝖪𝖺𝗍𝗌𝗎𝗄𝗂 𝖬𝖖',
-  '𝖡𝖺𝗄𝗎𝗀𝗈 𝖡𝗈𝗍'
+  'BAKI BOT',
+  'BAKI BOT',
+  'BAKI BOT',
+  'BAKI BOT',
+  'BAKI BOT',
+  'BAKI BOT',
+  'BANI BOT',
+  'BAKI BOT',
+  'BAKI BOT',
+  'BAKI BOT'
 ];
 
 global.botImages = ['https://cdn.dix.lat/me/0lkj_20260829-c91x-2p9h-f407.png', 'https://cdn.dix.lat/me/138d_20260829-c91x-hp1s-a51f.jpg', 'https://cdn.dix.lat/me/e9jz_20260829-c91x-4tv6-9982.jpg', 'https://cdn.dix.lat/me/8cit_20260829-c91x-zss9-92ac.jpg', 'https://cdn.dix.lat/me/oaq9_20260829-c91x-t2de-31a0.jpg'];*/
