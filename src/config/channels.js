a2d76fd16b561c181c8c90de7f22e07d05a06b66
segundocristian219-oss <segundocristian219@gmail.com>
2026-08-31 14:02:07 -0600

    Actualizar channels.js

--- src/config/channels.js
+++ src/config/channels.js
@@ -1,3 +1,3 @@
-global.my = { canal1: '120363406846602793@newsletter', canal2: '120363427487466660@newsletter' };
-global.ch = '120363406846602793@newsletter';
+global.my = { canal1: '120363430994445859@newsletter', canal2: '120363430994445859@newsletter' };
+global.ch = '120363430994445859@newsletter';
 global.rmr = String.fromCharCode(8206).repeat(850);
