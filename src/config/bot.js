32fd2067b7136e080237963e32024db368314576
segundocristian219-oss <segundocristian219@gmail.com>
2026-08-31 01:32:58 -0600

    Actualizar bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -11,7 +11,7 @@
   'BAKI BOT'
 ];
 
-global.botImages = ['https://cdn.dix.lat/me/0lkj_20260829-c91x-2p9h-f407.png', 'https://cdn.dix.lat/me/138d_20260829-c91x-hp1s-a51f.jpg', 'https://cdn.dix.lat/me/e9jz_20260829-c91x-4tv6-9982.jpg', 'https://cdn.dix.lat/me/8cit_20260829-c91x-zss9-92ac.jpg', 'https://cdn.dix.lat/me/oaq9_20260829-c91x-t2de-31a0.jpg'];*/
+global.botImages = ['https://cdn.dix.lat/me/kz9m_20260831-c91x-s6ho-7ae9.jpg', 'https://cdn.dix.lat/me/kz9m_20260831-c91x-s6ho-7ae9.jpg', 'https://cdn.dix.lat/me/kz9m_20260831-c91x-s6ho-7ae9.jpg', 'https://cdn.dix.lat/me/kz9m_20260831-c91x-s6ho-7ae9.jpg', 'https://cdn.dix.lat/me/kz9m_20260831-c91x-s6ho-7ae9.jpg'];*/
 
 global.botNames = ['❖ BAKI BOT ❖', '⧫ BAKI BOT ⧫', '𖦹 BAKI BOT 𖦹', '⌠ BAKI BOT ⌡'];
 global.botImages = ['https://cdn.dix.lat/me/b7f2e139-8235-44cc-9ebd-9bc7fc97c0a8.jpg', 'https://cdn.dix.lat/me/6b74fd28-ff3a-44bb-9392-f1de5d0d4e10.jpg'];
