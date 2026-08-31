a6d773704cb136e645c3049db26fc1326a4efd68
segundocristian219-oss <segundocristian219@gmail.com>
2026-08-31 01:36:11 -0600

    Actualizar bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -14,4 +14,4 @@
 global.botImages = ['https://cdn.dix.lat/me/kz9m_20260831-c91x-s6ho-7ae9.jpg', 'https://cdn.dix.lat/me/kz9m_20260831-c91x-s6ho-7ae9.jpg', 'https://cdn.dix.lat/me/kz9m_20260831-c91x-s6ho-7ae9.jpg', 'https://cdn.dix.lat/me/kz9m_20260831-c91x-s6ho-7ae9.jpg', 'https://cdn.dix.lat/me/kz9m_20260831-c91x-s6ho-7ae9.jpg'];*/
 
 global.botNames = ['❖ BAKI BOT ❖', '⧫ BAKI BOT ⧫', '𖦹 BAKI BOT 𖦹', '⌠ BAKI BOT ⌡'];
-global.botImages = ['https://cdn.dix.lat/me/b7f2e139-8235-44cc-9ebd-9bc7fc97c0a8.jpg', 'https://cdn.dix.lat/me/6b74fd28-ff3a-44bb-9392-f1de5d0d4e10.jpg'];
+global.botImages = ['https://cdn.dix.lat/me/kz9m_20260831-c91x-s6ho-7ae9.jpg', 'https://cdn.dix.lat/me/kz9m_20260831-c91x-s6ho-7ae9.jpg'];
